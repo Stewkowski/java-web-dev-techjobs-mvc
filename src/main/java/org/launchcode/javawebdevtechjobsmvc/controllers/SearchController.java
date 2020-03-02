10eea41e468028c5fd8d685f9e28010d523bbd41
Sarah <maelee3@gmail.com>
2020-03-01 19:21:24 -0600

    radios broken

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,22 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results")
+    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        if(searchTerm.toLowerCase().equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs With All: " + searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+
+        return ("search");
+    }
+
 }
